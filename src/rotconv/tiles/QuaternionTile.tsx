bf3dbeead6836fa880289d88d1d0d1fc7cdf98e6
jugglingcats <github@akirkpatrick.com>
2023-02-15 14:48:27 +0000

    feat: add negate option to QuaternionTile.tsx

--- src/rotconv/tiles/QuaternionTile.tsx
+++ src/rotconv/tiles/QuaternionTile.tsx
@@ -1,35 +1,15 @@
 import * as React from "react"
-import styled from "styled-components"
+import { useCallback, useEffect, useRef, useState } from "react"
 import { PrecisionInput } from "../../util/PrecisionInput"
 import { TransformationInput, useTransformation } from "../TransformationProvider"
 import { Quaternion } from "three"
-import { useCallback, useEffect, useRef, useState } from "react"
 import { StyledTile } from "./styles"
 import { DockToolbar, DockToolbarButtonGroup } from "../../util/DockToolbar"
-import { ReactComponent as CopyIcon } from "@material-symbols/svg-400/outlined/content_copy.svg"
-import { Button, Dropdown } from "antd"
 import { ToolbarButtonsPrecision } from "../../util/ToolbarButtonsPrecision"
 import { useTileContext } from "../../util/TileContextProvider"
-import { GlowbuzzerIcon } from "../../util/GlowbuzzerIcon"
 import { ToolbarButtonNormalize } from "../../util/ToolbarButtonNormalize"
-
-const StyledMenuItem = styled.div`
-    padding: 0 0 4px 0;
-
-    .title {
-        font-size: 0.9em;
-        color: rgba(0, 0, 0, 0.8);
-    }
-
-    .content {
-        font-family: monospace;
-        border: 1px solid rgba(0, 0, 0, 0.15);
-        border-radius: 4px;
-        background: rgba(0, 0, 0, 0.7);
-        padding: 4px 8px;
-        color: rgba(255, 255, 255, 0.7);
-    }
-`
+import { GlowbuzzerIcon } from "../../util/GlowbuzzerIcon"
+import { ReactComponent as NegateIcon } from "@material-symbols/svg-400/rounded/swap_horiz.svg"
 
 export const QuaternionTile = () => {
     const { input, quaternion, setQuaternion } = useTransformation()
@@ -79,43 +59,18 @@ export const QuaternionTile = () => {
         setQuaternion(normalized)
     }
 
-    const cols = ["x", "y", "z", "w"]
+    function negate() {
+        set_all(edited.map(v => -v))
+    }
 
-    const items = [
-        {
-            key: "1",
-            label: (
-                <StyledMenuItem>
-                    <div className="title">New THREE object</div>
-                    <div className="content">new THREE.Quaternion(0, 0, 0, 0, 1)</div>
-                </StyledMenuItem>
-            )
-        },
-        {
-            key: "2",
-            label: (
-                <StyledMenuItem>
-                    <div className="title">Javascript array (x, y, z, w)</div>
-                    <div className="content">[0, 0, 0, 0]</div>
-                </StyledMenuItem>
-            )
-        },
-        {
-            key: "3",
-            label: (
-                <StyledMenuItem>
-                    <div className="title">Javascript object</div>
-                    <div className="content">{`{x: 0, y: 0, z: 0, w: 0}`}</div>
-                </StyledMenuItem>
-            )
-        }
-    ]
+    const cols = ["x", "y", "z", "w"]
 
     return (
         <div>
             <DockToolbar>
                 <DockToolbarButtonGroup>
                     <ToolbarButtonNormalize onClick={normalize} />
+                    <GlowbuzzerIcon Icon={NegateIcon} button onClick={negate} title="Negate" />
                 </DockToolbarButtonGroup>
                 <ToolbarButtonsPrecision />
             </DockToolbar>
