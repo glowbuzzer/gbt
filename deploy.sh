de24870c6cb6d62a5eaacf5b2d82598ca71dc6c8
jugglingcats <github@akirkpatrick.com>
2023-02-16 09:46:52 +0000

    chore: increase retries for cloudfront

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,3 @@
 aws s3 sync --region eu-west-1 --quiet --delete ./dist "s3://$1" && \
+export AWS_MAX_ATTEMPTS=10 && \
 aws cloudfront create-invalidation --region eu-west-1 --paths "/*" --distribution-id $2
